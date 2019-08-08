ac863331b99608569c3d80692e8c412ba7765813
Evan Willner <46433705+daveempire@users.noreply.github.com>
2019-08-08 03:51:31 -0400

    Create SockMerchant.py

---
+++ Algorithms/Implementation/SockMerchant.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+# https://www.hackerrank.com/challenges/sock-merchant/problem
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the sockMerchant function below.
+def sockMerchant(n, ar):
+    for x in ar:
+        print(n)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
