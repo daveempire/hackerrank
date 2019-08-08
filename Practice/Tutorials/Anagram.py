bbd61f90b253c4c5dcb35c7d22c28aa59db77c5c
Evan Willner <46433705+daveempire@users.noreply.github.com>
2019-08-08 03:43:31 -0400

    Create Anagram.py

---
+++ Practice/Tutorials/Anagram.py
@@ -0,0 +1,45 @@
+#!/bin/python3
+
+import string
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the makeAnagram function below.
+def makeAnagram(a, b):
+
+    deletions = 0
+
+    # sets keys of dictionary equal to a-z with values of 0
+    aDict = dict.fromkeys(string.ascii_lowercase, 0)
+    bDict = dict.fromkeys(string.ascii_lowercase, 0)
+
+    # fill dictionary a
+    for x in a:
+        aDict[x] += 1
+
+    # fill dictionary b
+    for x in b:
+        bDict[x] += 1
+
+    # for letter count in alphabet, compare value at each letter in both dicts
+    for x in aDict:
+        if aDict[x] != bDict[x]:
+            deletions += abs(aDict[x] - bDict[x])
+
+    return deletions
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = input()
+
+    b = input()
+
+    res = makeAnagram(a, b)
+
+    fptr.write(str(res) + '\n')
+
+    fptr.close()
